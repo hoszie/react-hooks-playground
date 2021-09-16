2d826cf919851594aa8cbb5d2cc50f9e7d59c173
Nick Hoszko <nhoszko@gmail.com>
2021-09-16 19:55:03 +1000

    init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'; //useState to add state to functional components, useEffect like component did mount. 
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import NoteApp from './components/NoteApp'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NoteApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +15,37 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+// const App = (props) => {
+//   const [count, setCount] = useState(props.count)
+//   const [text, setText] = useState('')
+
+//   useEffect(() => {
+//     console.log('This should only run once')
+//   }, [])
+
+//   useEffect(() => {
+//     console.log('useEffect ran')
+//     document.title = count
+//   }, [count])
+
+//   return (
+//     <div>
+//       <p>The current {text || 'count'} is {count}</p>
+//       <button onClick={() => setCount(count + 1)}>+1</button>
+//       <button onClick={() => setCount(count - 1)}>-1</button>
+//       <button onClick={() => setCount(props.count)}>Reset</button>
+//       <input value={text} onChange={(e) => setText(e.target.value)}/>
+//     </div>
+//   )
+// }
+
+// App.defaultProps = {
+//   count: 0
+// }
+
+
+
+
